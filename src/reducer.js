191c90457a29b49bbcce5f8959b911144ca878cc
Joseph M <jmedina1985@hotmail.com>
2021-03-30 22:29:36 -0700

    Increase and decrease items in cart

--- src/reducer.js
+++ src/reducer.js
@@ -9,7 +9,12 @@ const reducer = (state, action) => {
     };
   }
   if (action.type === 'INCREASE') {
-    let tempCart = [];
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
     return { ...state, cart: tempCart };
   }
   return state;
