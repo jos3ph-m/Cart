459e959002e25dcb7148bc9e3efb91be7a7294ea
Joseph M <jmedina1985@hotmail.com>
2021-04-01 21:07:53 -0700

    setting up toggle

--- src/context.js
+++ src/context.js
@@ -38,6 +38,10 @@ const AppProvider = ({ children }) => {
     dispatch({ type: 'DISPLAY_ITEMS', payload: cart });
   };
 
+  const toggleAmount = (id, type) => {
+    dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } });
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
